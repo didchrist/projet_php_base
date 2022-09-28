938a2580c7b5c0b561b60f1ddd1d1be1b27f0aca
didchrist <christopher.pelette@gmail.com>
2022-09-28 13:53:31 +0200

    version avec des boucles

--- index.php
+++ index.php
@@ -2,13 +2,27 @@
 if (isset($_POST["age"])) {
     $age = $_POST["age"];
     if ($age >= 18) {
-        echo("Il peut conduire");
+        echo("Vous pouvez conduire");
     }
     elseif ($age >= 16) {
-        echo("Il peut passer uniquement la conduite accompagner");
+        echo("Vous pouvez passer uniquement la conduite accompagner");
     }
     else {
         echo("Pas de conduite en vue");
+    }
+    $tableau = [
+        "Robert" => 25,
+        "Jean" => 45,
+        "Marion" => 26,
+    ];
+    $message_Ensemble=[];
+    foreach ($tableau as $key => $age_Tableau) {
+        if ($age_Tableau>$age) {
+            array_push($message_Ensemble,["Vous etes plus jeune que" ." ".$key]);
+        }
+        else {
+            array_push($message_Ensemble,["Vous etes plus age que" ." ".$key]);
+        }
     }
 }
 ?>
@@ -26,6 +40,10 @@ if (isset($_POST["age"])) {
     <input type="number" name="age" id="name" required>
     <input type="submit" value="Valider">
     </form>
-
+    <?php if (isset($_POST["age"])): ?>
+        <?php foreach ($message_Ensemble as $message): ?>
+            <p><?php echo implode("",$message); ?></p>
+        <?php endforeach; ?>
+    <?php endif; ?>
 </body>
 </html>
